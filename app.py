3d23752b036bc0389007f2e5daf8c9b9751b1fff
rakumanakasu007 <rakumanakasu007@gmail.com>
2025-08-22 14:42:21 +0700

    commit fix

--- app.py
+++ app.py
@@ -27,3 +27,4 @@ def support():
 # ----------------------------
 if __name__ == '__main__':
     app.run(debug=True)
+
--- route/home.py
+++ route/home.py
@@ -1,29 +1,30 @@
 from flask import Blueprint, render_template
 from helper import product_db
+import requests
 
 home_bp = Blueprint("home", __name__)
+
 
 
 @home_bp.route('/')
 @home_bp.route('/home')
 def home():
     products = []
-    carousel_images = []
     error = ''
-
     try:
-        # fetch products from SQLite
-        products = product_db.get_products_from_db()
-
-        # Make sure rating fields exist
-        for p in products:
-            p.setdefault('rating_rate', 0)
-            p.setdefault('rating_count', 0)
-
-        # Get first 3 images for carousel
-        carousel_images = [('/photos/' + p['image']) if p['image'] else '/static/default.jpg'
-                           for p in products[:3]]
+        resp = requests.get("https://admindashboardflask-production-1a1e.up.railway.app/api/products")
+        if resp.status_code == 200:
+            products = resp.json()
+        else:
+            error = f"API error {resp.status_code}"
     except Exception as e:
         error = str(e)
+
+    for p in products:
+        p.setdefault('rating_rate', 0)
+        p.setdefault('rating_count', 0)
+
+    carousel_images = [('/photos/' + p['image']) if p['image'] else '/static/default.jpg'
+                       for p in products[:3]]
 
     return render_template('home.html', products=products, carousel_images=carousel_images, error=error)
